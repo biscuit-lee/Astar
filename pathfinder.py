7051e29e9966f89500bbc9e8058dfdf26df572dc
biscuit-lee <68615351+biscuit-lee@users.noreply.github.com>
2021-10-09 23:54:47 +0200

    implemented half the Astar algorithm

--- pathfinder.py
+++ pathfinder.py
@@ -22,9 +22,9 @@ def main():
     pg.font.init()
     #define font
     myfont = pg.font.SysFont('Comic Sans MS', 11)
-    startFont =myfont.render("Start",False,(0,0,0))
-    goalFont =myfont.render("Goal",False,(0,0,0)) 
-    obFont =myfont.render("Obstacle",False,(0,0,0)) 
+    startFont = myfont.render("Start",False,(0,0,0))
+    goalFont = myfont.render("Goal",False,(0,0,0)) 
+    obFont = myfont.render("Simulate",False,(0,0,0)) 
     pg.init()
     mode = 1
     screen = pg.display.set_mode([400,300])
@@ -41,7 +41,7 @@ def main():
                 
                 
                 #if event.type==pg.NOUSEBUTTONDOWN:
-                if pg.mouse.get_pressed()[0]:#MOUSEBUTTONDOWN:
+                if pg.mouse.get_pressed()[0]:
 
                     pos = pg.mouse.get_pos()
                     pos_x = pos[0]//(20+5)
@@ -57,10 +57,13 @@ def main():
                         #goal button
                         elif(350 < pos[0] < 390) and (20 < pos[1] < 130):
                             mode = 3
-                        #obstacle button
+                        # start the simulation button button
                         elif(350 < pos[0] < 390) and (20 < pos[1] < 210):
-                            mode = 1
-                
+                            
+                            # fetch the start and goal position
+                            start_pos = find_start(grid)
+                            goal_pos = find_goal(grid)
+                            pathfinder(grid,start_pos,goal_pos)
         screen.fill(WHITE)
         
         drawGrid(screen,BLACK,20,5,5,grid)
@@ -102,25 +105,87 @@ class Node():
         self.h = 0
         self.g = 0
 
+    # function used to override pythons default object comparison
+    
+    def __eq__(self,other):
+        if self.position == other.position:
+            return True
+        else:
+            return False
+
+# function to find the start points position
+ 
+def find_start(grid):
+    for i in range(len(grid)):
+        for z in range(len(grid[0])):
+            if grid[i][z] == 2:
+                return (i,z)
+# function to find the goals position
+def find_goal(grid):
+    for i in range(len(grid)):
+        for z in range(len(grid[0])):
+            if grid[i][z] == 3:
+                return (i,z)
 
 def pathfinder(grid,start_pos,goal_pos):
     # open list and close list
     open_list = []
     close_list = []
-
+    children_pos = ((-1,0),(0,-1),(1,1),(-1,-1),(-1,1),(1,-1),(0,1),(1,0))
     # make the start and goal an object
     start = Node(start_pos)
     goal = Node(goal_pos)
-
+    start.g = 0
+    start.h = (start.position[0] - goal.position[0])**2 + (start.position[1] - goal.position[1])**2
+    start.f = start.h
     open_list.append(start)
+    current = open_list[0]
     while (len(open_list) > 0):
-        current = open_list[0]
+        # find node with lowest f value and make it curreent note to expand on later 
         for count,item in enumerate(open_list):
             if item.f < current.f:
-                close_list.append(current)
                 current = item
+                close_list.append(current)
                 open_list.pop(count)
+        
+        # if found goal
+        if current == goal:
+            # reverse the path and color it
+            print("FOUND GOAL")
+            tmp = current.parent
+            while (tmp.parent is not None):
+                print("tmp = ",tmp.position)
+                grid[tmp.position[0]][tmp.position[1]] = 4
+                tmp = tmp.parent
+            break
+        # generate child
+        for child in children_pos:
+                y_pos = child[0] + current.position[0]
+                x_pos = child[1] + current.position[1]
+
+                if 13 < x_pos or x_pos < 0:
+                    continue
+                if 13 < y_pos or y_pos < 0:
+                    continue
+
+                childNode = Node((y_pos,x_pos),current)
+                # check if child is already visited 
+                for node in close_list:
+                    if node == childNode:
+                        continue
+
+                childNode.g = (abs(start.position[0] - childNode.position[0])**2) + (abs(start.position[1] - childNode.position[1])**2)
+                childNode.h = (abs(goal.position[0] - childNode.position[0])**2) + (abs(goal.position[1] - childNode.position[1])**2)
+                childNode.f = childNode.h
                 
+                # check if child is in openlist
+                for node in open_list:
+                    if node == childNode:
+                            if childNode.f > current.f:
+                     
+                                continue
+                    
+                open_list.append(childNode)
 
 #draw the grids on the window
 def drawGrid(win,color,size,sx,sy,grid):
@@ -135,6 +200,8 @@ def drawGrid(win,color,size,sx,sy,grid):
                 color = GREEN
             elif grid[y][x] == 3:
                 color = YELLOW
+            elif grid[y][x] == 4:
+                color = WHITE
             else:
                 color=(0,0,0)
             rect = pg.Rect((x*size+sxx),(y*size+syy),size,size)
